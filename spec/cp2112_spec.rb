1d5352be70af0d46777b621290349aa139be8945
fenrir <fenrir.naru@gmail.com>
2020-01-30 00:10:54 +0900

    Remove failure case

--- spec/cp2112_spec.rb
+++ spec/cp2112_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe CP2112 do
   it "has a version number" do
     expect(CP2112::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
